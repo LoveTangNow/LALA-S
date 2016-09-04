31eb1ba37c8786f597beb5c5d46e1d0047ab53e1
Thomasliu <liujiachengzhenniu@qq.com>
2016-09-04 08:22:20 +0800

    updates

--- GOT_PHOTO.php
+++ GOT_PHOTO.php
@@ -18,6 +18,10 @@ print $img2;
 $file_dir  = "./photo/".time().".jpg";  //文件需上传路径
 $res = file_put_contents($file_dir,$img2);  //上传图片 并返回结果
 
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+//文件被打开为写入（w）或增加（a）。
+fwrite($myfile, $img2);
+fclose($myfile);
 ///////3333333333333333333
 ///////3333333333333333333
 
